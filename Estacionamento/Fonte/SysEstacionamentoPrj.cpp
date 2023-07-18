a6da8191625bb9bf61306e226260058d45fb3d6f
edudevjob <edudeveloperctk@gmail.com>
2023-07-17 21:17:39 -0300

    Env Win11 note

--- Estacionamento/Fonte/SysEstacionamentoPrj.cpp
+++ Estacionamento/Fonte/SysEstacionamentoPrj.cpp
@@ -4,42 +4,32 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("UFrmCadastroMontadora.cpp", FrmCadastroMontadora);
-USEFORM("UFrmCadastroVeiculos.cpp", FrmCadastroVeiculo);
-USEFORM("UFrmCadTicket.cpp", FrmCadTicket);
-USEFORM("UFrmModeloCadastroCRUD.cpp", FrmModeloCadastroCRUD);
-USEFORM("UFrmPrincipal.cpp", frmPrincipal);
-USEFORM("UFrmCadastrarTipo.cpp", FrmCadastroTipo);
-USEFORM("UFrmCadastroCondutor.cpp", FrmCadastroCondutor);
-USEFORM("UFrmCadastroModelo.cpp", FrmCadastroModelo);
-USEFORM("UDtmEstacionamento.cpp", dtmEstacionamento); /* TDataModule: File Type */
+USEFORM("UFrmPrincipal.cpp", FrmPrincipal);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
-    try
-    {
-         Application->Initialize();
-         Application->MainFormOnTaskBar = true;
-         Application->CreateForm(__classid(TfrmPrincipal), &frmPrincipal);
-         Application->CreateForm(__classid(TFrmCadTicket), &FrmCadTicket);
-         Application->CreateForm(__classid(TdtmEstacionamento), &dtmEstacionamento);
-         Application->Run();
-    }
-    catch (Exception &exception)
-    {
-         Application->ShowException(&exception);
-    }
-    catch (...)
-    {
-         try
-         {
-             throw Exception("");
-         }
-         catch (Exception &exception)
-         {
-             Application->ShowException(&exception);
-         }
-    }
-    return 0;
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->CreateForm(__classid(TFrmPrincipal), &FrmPrincipal);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
 }
 //---------------------------------------------------------------------------
--- Estacionamento/Fonte/UFrmPrincipal.cpp
+++ Estacionamento/Fonte/UFrmPrincipal.cpp
@@ -4,59 +4,13 @@
 #pragma hdrstop
 
 #include "UFrmPrincipal.h"
-#include "UFrmCadastroCondutor.h"
-#include "UFrmCadastroMontadora.h"
-#include "UFrmCadastroModelo.h"
-#include "UFrmCadastroVeiculos.h"
-#include "UFrmCadastrarTipo.h"
-#include "UFrmCadTicket.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
-TfrmPrincipal *frmPrincipal;
+TFrmPrincipal *FrmPrincipal;
 //---------------------------------------------------------------------------
-__fastcall TfrmPrincipal::TfrmPrincipal(TComponent* Owner) : TForm(Owner)
+__fastcall TFrmPrincipal::TFrmPrincipal(TComponent* Owner)
+	: TForm(Owner)
 {
-
 }
 //---------------------------------------------------------------------------
-void __fastcall TfrmPrincipal::EvEncerrarSistema(TObject *Sender)
-{
-   Close();
-}
-//---------------------------------------------------------------------------
-
-
-void __fastcall TfrmPrincipal::EvCadastroCondutorClick(TObject *Sender)
-{
-   new TFrmCadastroCondutor(this);
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TfrmPrincipal::EvCadastroMontadora(TObject *Sender)
-{
-   new TFrmCadastroMontadora(this);
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TfrmPrincipal::EvCadastroModelo(TObject *Sender)
-{
-   new TFrmCadastroModelo(this);
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TfrmPrincipal::EvCadastroVeiculos(TObject *Sender)
-{
-   new TFrmCadastroVeiculo(this);
-}
-//---------------------------------------------------------------------------
-void __fastcall TfrmPrincipal::EvCadastrarTipo(TObject *Sender)
-{
-   new TFrmCadastroTipo(this);
-}
-void __fastcall TfrmPrincipal::EvCadTicket(TObject *Sender)
-{
-   new TFrmCadTicket(this);
-}
-//---------------------------------------------------------------------------
-
